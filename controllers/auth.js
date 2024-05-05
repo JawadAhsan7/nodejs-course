48ab7fed5934c24a2a19c97451d3a0def7eacbd3
Jawad Ahsan <jahsandev@gmail.com>
2024-05-05 19:15:19 +0500

    move jwt creation logic to the UserSchema/model file - with instance methods

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,15 +1,9 @@
-const { BadRequestError } = require("../errors");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
-    const token = await jwt.sign(
-        { userId: user._id, name: user.name },
-        process.env.JWT_SECRET,
-        { expiresIn: "30d" },
-    );
+    const token = user.createJWT();
     res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
