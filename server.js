439534c84b1884afd22f29148e3acf9e59c124f0
Jawad Ahsan <jahsandev@gmail.com>
2024-05-05 15:27:53 +0500

    setup route middlewares in server.js - authRouter, jobsRouter

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ const notFound = require("./middlewares/not-found");
 const errorHandlerMiddleware = require("./middlewares/error-handler");
 const connectDB = require("./db/connect");
 
+// ROUTERS
+const authRouter = require("./routes/auth");
+const jobsRouter = require("./routes/jobs");
+
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const JWT_SECRET = process.env.JWT_SECRET;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// ROUTES
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/jobs", jobsRouter);
 
 app.use(notFound);
 app.use(errorHandlerMiddleware);
