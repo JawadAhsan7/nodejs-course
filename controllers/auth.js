9d4b87e76f22cc594b059650a827452cd1692f0b
Jawad Ahsan <jahsandev@gmail.com>
2024-05-05 19:02:34 +0500

    generate token in register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,16 @@
 const { BadRequestError } = require("../errors");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 const { StatusCodes } = require("http-status-codes");
 
 const register = async (req, res) => {
-    const { name, email, password } = req.body;
-
-    if (!name || !email || !password) {
-        throw new BadRequestError("Please provide name, email and password");
-    }
-
-    const user = await User.create({ name, email, password });
-    res.status(StatusCodes.CREATED).json({ user });
+    const user = await User.create({ ...req.body });
+    const token = await jwt.sign(
+        { userId: user._id, name: user.name },
+        process.env.JWT_SECRET,
+        { expiresIn: "30d" },
+    );
+    res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
